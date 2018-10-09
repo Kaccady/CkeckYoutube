c374961d45e80c09b90c16616a0f43afb82ac113
Kaccady <z-rz@mail.com>
2018-10-09 15:52:35 +0300

    work

--- src/Checkform.jsx
+++ src/Checkform.jsx
@@ -1,35 +1,33 @@
 import React from 'react';
-   
-class CheckForm extends React.Component{
+
+class CheckForm extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {showed: true,
-            videoId: 'https://img.youtube.com/vi/YqeW9_5kURI/0.jpg',
-            imageSource: ''};
-        this.getImage = this.getImage.bind(this);
-        this.onLinkChange = this.onLinkChange.bind(this);
+        this.state = {
+            showed: false,
+            linkOnVideo: 'https://www.youtube.com/watch?v=YqeW9_5kURI',
+            videoId: '',
+            imageSource: 'https://img.youtube.com/vi/YqeW9_5kURI/0.jpg',
 
+        }
     };
-    getImage(){
-        this.setState({ imageSource: this.state.videoId});
-        this.setState({ showed: false});
 
+    onLinkChange = (event) => {
+        this.setState({ linkOnVideo: event.target.value });
+        this.setState({ imageSource: 'https://img.youtube.com/vi/' + event.target.value.replace("https://www.youtube.com/watch?v=", "") + '/0.jpg' });
+        this.setState({ showed: false });
+        
+    };
 
-    };
-    onLinkChange(event){
-        this.setState({videoId: event.target.value});
-    };
-    render() { 
-    return (
-    <div>
-    <form>
-        <p>Работоспособность ссылки на YouTube:</p><br/>
-        <input value={this.state.videoId} size='52' onChange={this.onLinkChange}  type='text'/>
-        <button onClick={this.getImage} type='button'>Проверить</button><br/>
-        </form>
-        <img hidden={this.state.showed} src={this.state.imageSource} alt='youtubeImage' id='youtubePic'/>
-        </div>
-    );
-    }}
-   export default CheckForm;
-   
+    render() {
+        return (
+            <div>
+                <p>Получить заставку с YouTube видео:</p><br />
+                <input placeholder='ссылка на видео' value={this.state.linkOnVideo} size='64' onChange={this.onLinkChange} type='text' /><br/><br/>
+                <img hidden={this.state.showed} src={this.state.imageSource} alt='youtubeImage' id='youtubePic' />
+            </div>
+        );
+    }
+}
+
+export default CheckForm;
