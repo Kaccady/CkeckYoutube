937b84d099d39d24c5cded395935eb7217432687
Kaccady <z-rz@mail.com>
2018-10-09 12:19:38 +0300

    +-

--- src/Checkform.jsx
+++ src/Checkform.jsx
@@ -3,22 +3,31 @@ import React from 'react';
 class CheckForm extends React.Component{
     constructor(props) {
         super(props);
+        this.state = {showed: false};
         this.state = {videoId: 'https://img.youtube.com/vi/YqeW9_5kURI/0.jpg'};
+        this.state = {imageSource: 'https://img.youtube.com/vi/YqeW9_5kURI/0.jpg'};
         this.getImage = this.getImage.bind(this);
-    }
-    getImage(){
-        this.setState({ videoId: 'https://img.youtube.com/vi/YqeW9_5kURI/1.jpg'})
+        this.onLinkChange = this.onLinkChange.bind(this);
 
-    }
+    };
+    getImage(){
+        this.setState({ imageSource: this.state.videoId});
+        this.setState({ showed: false});
+
+
+    };
+    onLinkChange(event){
+        this.setState({videoId: event.target.value});
+    };
     render() { 
     return (
     <div>
     <form>
         <p>Работоспособность ссылки на YouTube:</p><br/>
-        <input type='text'/>
+        <input value={this.state.videoId} size='52' onChange={this.onLinkChange}  type='text'/>
         <button onClick={this.getImage} type='button'>Проверить</button><br/>
         </form>
-        <img src={this.state.videoId} alt='youtubeImage' id='youtubePic'/>
+        <img hidden={this.state.showed} src={this.state.imageSource} alt='youtubeImage' id='youtubePic'/>
         </div>
     );
     }}
