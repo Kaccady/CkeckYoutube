a9f579dda8ebed78796a45dee86b0a74b5a06220
Kaccady <z-rz@mail.com>
2018-10-10 14:49:11 +0300

    e

--- src/Checkform.jsx
+++ src/Checkform.jsx
@@ -4,38 +4,35 @@ class CheckForm extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            showed: true,
+            hiddePic: false,
             linkOnVideo: 'https://www.youtube.com/watch?v=YqeW9_5kURI',
             imageSource: '',
-            errorsW: [],
+            errorsW: '',
         }
+    };
+    onWrongLink = () =>{
+        this.setState({hiddePic: true})
+};
+
+    onCorrectLink = () => {
+        this.setState({hiddePic: false})
     };
 
     onLinkChange = (event) => {
-        this.setState({ linkOnVideo: event.target.value });
-        this.setState({ imageSource: 'https://img.youtube.com/vi/' + 
+        this.setState({ linkOnVideo: event.target.value, imageSource: 'https://img.youtube.com/vi/' + 
         event.target.value.replace("https://www.youtube.com/watch?v=", "") + 
         '/0.jpg' });
-        fetch(this.state.imageSource,{mode: 'cors', 
-        headers:'Authorization: Bearer AIzaSyDq2afP16hclKa6dPbmpiPUdr4vjZFr_CI'})
-        .then(res => res.text())
-        .then((errorsW) =>{
-            this.setState({
-                errorsW
-            })
-        })
-
-        if(this.state.errors === 44){
-            this.setState({})
-        }else{this.setState({showed: false})}
-        };
+        const headers = new Headers()
+        headers.set('Authorization', 'Bearer AIzaSyDq2afP16hclKa6dPbmpiPUdr4vjZFr_CI')
+        fetch(this.state.imageSource,{mode: 'cors', headers})
+    };
 
     render() {
         return (
             <div>
                 <p>Получить заставку с YouTube видео:</p><br />
                 <input placeholder='ссылка на видео' value={this.state.linkOnVideo} size='64' onChange={this.onLinkChange} type='text' /><br/><br/>
-                <img hidden={this.state.showed} src={this.state.imageSource} alt='youtubeImage' id='youtubePic' />
+                <img hidden={this.state.hiddePic} onLoad={this.onCorrectLink} onError={this.onWrongLink} src={this.state.imageSource} alt='youtubeImage' id='youtubePic' />
                 <p>{}</p>
             </div>
         );
