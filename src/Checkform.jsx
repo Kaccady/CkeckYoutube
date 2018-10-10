27c229d21516acaed888a0bb25dfa245b2393e53
Kaccady <z-rz@mail.com>
2018-10-10 16:47:33 +0300

    done

--- src/Checkform.jsx
+++ src/Checkform.jsx
@@ -4,35 +4,32 @@ class CheckForm extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            hiddePic: false,
-            linkOnVideo: 'https://www.youtube.com/watch?v=YqeW9_5kURI',
-            imageSource: '',
+            hidePic: false,
+            linkOnVideo: '',
+            imageSource: 'https://img.youtube.com/vi/YqeW9_5kURI/0.jpg',
             errorsW: '',
         }
     };
     onWrongLink = () =>{
-        this.setState({hiddePic: true})
+        this.setState({hidePic: true})
 };
 
     onCorrectLink = () => {
-        this.setState({hiddePic: false})
+        this.setState({hidePic: false})
     };
 
     onLinkChange = (event) => {
         this.setState({ linkOnVideo: event.target.value, imageSource: 'https://img.youtube.com/vi/' + 
         event.target.value.replace("https://www.youtube.com/watch?v=", "") + 
-        '/0.jpg' });
-        const headers = new Headers()
-        headers.set('Authorization', 'Bearer AIzaSyDq2afP16hclKa6dPbmpiPUdr4vjZFr_CI')
-        fetch(this.state.imageSource,{mode: 'cors', headers})
+        '/0.jpg', hidePic: false });
     };
 
     render() {
         return (
             <div>
-                <p>Получить заставку с YouTube видео:</p><br />
+                <h1>Получить заставку с YouTube видео:</h1><br />
                 <input placeholder='ссылка на видео' value={this.state.linkOnVideo} size='64' onChange={this.onLinkChange} type='text' /><br/><br/>
-                <img hidden={this.state.hiddePic} onLoad={this.onCorrectLink} onError={this.onWrongLink} src={this.state.imageSource} alt='youtubeImage' id='youtubePic' />
+                <img hidden={this.state.hidePic} src={this.state.imageSource} alt='youtubeImage' id='youtubePic' />
                 <p>{}</p>
             </div>
         );
